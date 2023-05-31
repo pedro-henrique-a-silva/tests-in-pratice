0bff6bc29b95119a3171611269905467108ad5ac
Pedro Henrique A. Silva <pedro.henrique.a.s66@gmail.com>
2023-05-31 15:47:58 -0300

    Primeiro commit

--- tests/sum.spec.js
+++ tests/sum.spec.js
@@ -4,7 +4,7 @@ describe('1 - Crie os casos de teste da funcão `Sum`', () => {
 
   it ('Testa se ao receber "4" e "5" como parâmetro, retorna "9" como resultado', () => {
     // Remova o fail e escreva seus testes abaixo
-    fail('Teste vazio!');
+    fail('Teste vazio');
   });
 
   it ('Testa se ao receber "0" nos dois parâmetros, retorna o resultado "0"', () => {
