275db384b9a4c9d6a437c411dc7de02cee21351c
Pedro Henrique A. Silva <pedro.henrique.a.s66@gmail.com>
2023-05-31 16:30:23 -0300

    Requisitos ate o 4 resolvido

--- src/printMessage.js
+++ src/printMessage.js
@@ -5,12 +5,20 @@ let info = {
 };
 
 const printMessage = (characterInfo) => {
+  if (typeof characterInfo !== 'object' || (!Object.keys(characterInfo).includes('personagem'))) {
+    throw new Error('objeto inválido.')
+  }
   // Implemente seu código aqui
   let result = `Boas vindas, ${characterInfo.personagem}`;
 
   return result;
 };
 
-console.log(printMessage(info));
+try {
+  console.log(printMessage('ola mundo'));
+
+} catch (e) {
+  console.log(e.message);
+}
 
 module.exports = { info, printMessage };
